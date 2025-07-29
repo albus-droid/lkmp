14696780e41ba914644a0f62b336eebeb68d56dc
albus-droid <albinbabuvarghese20@gmail.com>
2025-07-29 19:05:38 -0400

    Added GPL and completed the module

--- hello-world-kmod/hello_world.c
+++ hello-world-kmod/hello_world.c
@@ -1,7 +1,18 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
-int int_module(void) {
-   printk(KERN_ALERT "Hello ,World!");
+static int __init hello_init(void) {
+   printk(KERN_ALERT "Hello, World!");
    return 0;
 }
+
+static void __exit bye_exit(void) {
+  printk(KERN_ALERT "Bye, World!");
+}
+
+module_init(hello_init);
+module_exit(bye_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("A module that prints Hello, World!");
